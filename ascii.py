3adee852995bc98d78e905e2550a69f9914a6e4c
Pranav Prakash <pranavpprakash7@gmail.com>
2024-01-01 21:24:25 +0530

    better_ui

--- ascii.py
+++ ascii.py
@@ -22,7 +22,10 @@ class ASCIIConverterApp:
         self.output_placeholder_image = Image.new("RGB", (400, 400), "white")
 
         self.image_frame = tk.Frame(self.root)
-        self.image_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        self.image_frame.grid(row=0, column=0, padx=10, pady=10)
+
+        self.side_bar = tk.Frame(self.root)
+        self.side_bar.grid(row=0, column=1, padx=10, pady=10)
 
         self.create_widgets()
         self.root.update_idletasks()
@@ -32,44 +35,49 @@ class ASCIIConverterApp:
         self.show_input_image(self.input_placeholder_image, parent_frame=self.image_frame)
         self.show_output_image(self.output_placeholder_image, parent_frame=self.image_frame)
 
-        main_frame = tk.Frame(self.root)
-        main_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        browse_input_frame = tk.Frame(self.side_bar)
+        browse_input_frame.pack(pady=5)
 
-        browse_input_frame = tk.Frame(main_frame)
-        browse_input_frame.grid(row=0, column=0, pady=5)
+        tk.Label(browse_input_frame, text="Browse Input Image:", anchor="w").pack(side="top", fill="x")
 
-        tk.Label(browse_input_frame, text="Browse Input Image:").pack(side="left")
-        self.input_image_path_entry = tk.Entry(browse_input_frame, width=40, state="readonly")
-        self.input_image_path_entry.pack(side="left", padx=5)
-        tk.Button(browse_input_frame, text="Select Image", command=self.browse_input_image).pack(side="left", padx=5)
+        path_button_frame = tk.Frame(browse_input_frame)
+        path_button_frame.pack(side="top", fill="x")
 
-        browse_output_frame = tk.Frame(main_frame)
-        browse_output_frame.grid(row=1, pady=5)
+        self.input_image_path_entry = tk.Entry(path_button_frame, width=40, state="readonly")
+        self.input_image_path_entry.pack(side="left", padx=5, fill="x")
 
-        tk.Label(browse_output_frame, text="Select Output Folder:").pack(side="left")
+        tk.Button(path_button_frame, text="Select Image", command=self.browse_input_image).pack(side="left", padx=5)
+
+        browse_output_frame = tk.Frame(self.side_bar)
+        browse_output_frame.pack(side="top", fill="x")
+
+        tk.Label(browse_output_frame, text="Select Output Folder:", anchor="w").pack(side="top", fill="x")
+
         self.output_folder_path_entry = tk.Entry(browse_output_frame, width=40, state="readonly")
-        self.output_folder_path_entry.pack(side="left", padx=5)
+        self.output_folder_path_entry.pack(side="left", padx=5, fill="x")
+
         tk.Button(browse_output_frame, text="Select Folder", command=self.browse_output_folder).pack(side="left", padx=5)
 
-        range_width_frame = tk.Frame(main_frame)
-        range_width_frame.grid(row=2, pady=5)
+        range_width_frame = tk.Frame(self.side_bar)
+        range_width_frame.pack(pady=5)
 
-        tk.Label(range_width_frame, text="Range Width:").pack(side="left")
+        tk.Label(range_width_frame, text="Range Width:", anchor="w").pack(side="top", fill="x")
+
         scale = tk.Scale(range_width_frame, variable=self.range_width, from_=1.0, to=50.0, orient="horizontal", resolution=1.0, showvalue=False, label=None)
-        scale.pack(side="left", padx=5)
-        tk.Label(range_width_frame, width=2, textvariable=self.range_width).pack(side="left", padx=2)
+        scale.pack(side="top", padx=5, fill="x")
+        tk.Label(range_width_frame, width=2, textvariable=self.range_width).pack(side="top", padx=2)
 
-        font_size_frame = tk.Frame(main_frame)
-        font_size_frame.grid(row=3, pady=5)
+        font_size_frame = tk.Frame(self.side_bar)
+        font_size_frame.pack(pady=5)
 
-        tk.Label(font_size_frame, text="Font Size:").pack(side="left")
+        tk.Label(font_size_frame, text="Font Size:", anchor="w").pack(side="top", fill="x")
+
         font_size_scale = tk.Scale(font_size_frame, variable=self.font_size, from_=5, to=15, orient="horizontal", resolution=1, showvalue=False, label=None)
-        font_size_scale.pack(side="left", padx=5)
-        tk.Label(font_size_frame, width=2, textvariable=self.font_size).pack(side="left", padx=2)
+        font_size_scale.pack(side="top", padx=5, fill="x")
+        tk.Label(font_size_frame, width=2, textvariable=self.font_size).pack(side="top", padx=2)
 
-        tk.Button(main_frame, text="Convert", command=self.convert_image).grid(row=4, pady=10)
+        tk.Button(self.side_bar, text="Convert", command=self.convert_image).pack(pady=10, fill="x")
 
-        
 
     def update_input_image_path_entry(self, path):
         self.input_image_path_entry.config(state="normal")
